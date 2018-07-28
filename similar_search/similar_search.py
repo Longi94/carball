5cfc5b184f4d4d738660064968a61d50d3112b78
dtracers <dtracers@gmail.com>
2018-07-27 23:23:41 -0600

    added creation of hashes for search

---
+++ similar_search/similar_search.py
@@ -0,0 +1,76 @@
+sizes_per_layer = [7, 7, 5]
+num_layers = 3
+
+class SimilarSearch:
+
+    @staticmethod
+    def create_layers_from_point(point) -> tuple:
+        output = []
+        for i in range(num_layers):
+            layer_output = []
+            for j in range(len(point)):
+                divsor = max(1, sizes_per_layer[j] * i)
+                layer_coordinate = (point[j] // divsor) % (sizes_per_layer[j] * (i + 1))
+                layer_output.append(layer_coordinate)
+            output.append(tuple(layer_output))
+        return tuple(output)
+
+    @staticmethod
+    def compare_cars(car1, car2):
+        if car1.team != car2.team:
+            return car2.team - car1.team
+        if car1.position.x != car2.position.x:
+            return car2.position.x - car1.position.x
+        if car1.position.y != car2.position.y:
+            return car2.position.y - car1.position.y
+        if car1.position.z != car2.position.z:
+            return car2.position.z - car1.position.z
+        return 0
+
+    @staticmethod
+    def create_layers_from_positions(ball, car_positions):
+        """
+        Creates the hash of the position that can be used for searching.
+        :param ball: has a field called: position
+        :param car_positions: has a fields called: team, position
+        :return: A list of hashes equal to the number of layers
+        """
+        # sort the list so that the hash is consistent
+        car_positions.sort(cmp=lambda car1, car2: SimilarSearch.compare_cars(car1, car2))
+
+        total_list = [ball] + car_positions
+        layer_list = [[], [], []]
+        for i in range(len(total_list)):
+            position = [total_list[i].position.x, total_list[i].position.y, total_list[i].position.z]
+            layers = SimilarSearch.create_layers_from_point(position)
+            for j in range(len(layers)):
+                layer_list[j].append(layers[j])
+
+        return layer_list
+
+    @staticmethod
+    def create_hashes_from_positions(ball, car_positions):
+        """
+        Creates the hash of the position that can be used for searching.
+        :param ball: has a field called: position
+        :param car_positions: has a fields called: team, position
+        :return: A list of hashes equal to the number of layers
+        """
+        # sort the list so that the hash is consistent
+        car_positions.sort(cmp=lambda car1, car2: SimilarSearch.compare_cars(car1, car2))
+
+        total_list = [ball] + car_positions
+        layer_list = [[], [], []]
+        hashes = []
+        for i in range(len(total_list)):
+            position = [total_list[i].position.x, total_list[i].position.y, total_list[i].position.z]
+            layers = SimilarSearch.create_layers_from_point(position)
+            for j in range(len(layers)):
+                layer_list[j].append(layers[j])
+
+        for i in range(len(layer_list)):
+            hashes.append(hash(tuple(layer_list[i])))
+
+        return hashes
+
+    def create_hashes_for_search(self, ):
